019055440dc87b0448550afc3b1a310b86d4e64c
ternlef11 <aliarazzak@hotmail.com>
2021-01-29 19:26:15 +0000

    Struggling with contacts :japanese_goblin:

--- AbuMoolah_29JAN21.py
+++ AbuMoolah_29JAN21.py
@@ -20,28 +20,74 @@ from tkinter.font import Font
 import os.path, pathlib, random, time, os, sys
 from datetime import datetime
 
+
+########### FUNCTIONS ##################################
+def contact_page():
+    master.destroy()
+    contacts = Tk()
+    contacts.wm_title("AbuMoola - Contacts")
+    #contacts.geometry('1000x800'.format(1000, 800))
+    contacts.configure(bg = 'black')
+
+    Label(contacts, font = ("Times", 120), foreground = 'white', bg = 'black', text = user_alias + " Contacts").grid(row = 0, column = 1)
+
+    contacts.grid_rowconfigure(1, minsize = 30)
+
+    Label(contacts, font = ("Times", 50), foreground = 'white', bg = 'black', text = "Format: FirstName,LastName,Address,#1,#2,Email,Insta,Extra").grid(row = 1, column = 1)
+    contacts.grid_rowconfigure(3, minsize = 30)
+    #con_add = Button(contacts, text="+", font=("system", 60), width = 10, height = 2)
+    #con_add.grid(row=2, column=1, columnspan = 5)
+
+    Button(contacts, font = ("Times", 50), width = 5, text = "Add", command = contact_add).grid(row = 2, column = 0)
+    con_add = Entry(contacts, font = ('Verdana',60), width = 40).grid(row = 2, column = 1)
+
+    Button(contacts, font = ("Times", 50), width = 5, text = "Remove", command = contact_remove).grid(row = 3, column = 0)
+    con_add = Entry(contacts, font = ('Verdana',60), width = 20).grid(row = 3, column = 1)
+
+def contact_add():
+    print("nice")
+
+def main_clock():
+    global time1
+    time2 = time.strftime("%H:%M:%S")
+    if time2 != time1:
+        time1=time2
+        the_time.config(text=time2, width = 10)
+    the_time.after(200, main_clock)
+
+
+########### PREPARTAION ##################################
+
 working_directory = pathlib.Path(__file__).parent.absolute()
 user_detail = str(working_directory) + "/user_data/"
 
 try:
-	user_detail = open(user_detail + "/user_details.txt", "r")
+    user_detail = open(user_detail + "/user_details.txt", "r")
 except:
-	print("Please provide details (will need to enter once)")
-	your_alias = input("How would you like Abu Moolah to address you? (< 20 characters please): ")
-	while len(your_alias) > 20:
-		your_alias = input("How would you like Abu Moolah to address you? (< 20 characters please): ")
-	detail_file = os.path.join(user_detail + "user_details.txt")
-	detail_file_open = open(detail_file, "w")
-	detail_file_open.write("HATHAHEA-ALIAS " + your_alias)
-	detail_file_open.close()
+    print("Please provide details (will need to enter once)")
+    your_alias = input("How would you like Abu Moolah to address you? (< 20 characters please): ")
+    while len(your_alias) > 20:
+        your_alias = input("How would you like Abu Moolah to address you? (< 20 characters please): ")
+    detail_file = os.path.join(user_detail + "user_details.txt")
+    detail_file_open = open(detail_file, "w")
+    detail_file_open.write("HATHAHEA-ALIAS " + your_alias)
+    detail_file_open.close()
 
 for line in user_detail:
-	if "HATHAHEA-ALIAS" in line:
-		user_alias = line.split()[1]
+    if "HATHAHEA-ALIAS" in line:
+        user_alias = line.split()[1]
 
 with open(str(working_directory) + "/random_deco/greetings.txt", "r") as the_greetings:
-	greetings_contents = the_greetings.read().split("\n")
-	the_greeting = random.choice(greetings_contents[:-1])
+    greetings_contents = the_greetings.read().split("\n")
+    the_greeting = random.choice(greetings_contents[:-1])
+
+    #def add_con():
+        #add_this =
+    #con_add = Button(contacts, text="+", font=("system", 60), width = 10, height = 2)
+    #con_add.grid(row=2, column=1, columnspan = 5)
+
+    #con_sub = Button(contacts, text="-", font=("system", 60), width = 10, height = 2)
+    #con_sub.grid(row=2, column=5, columnspan = 5)
 
 master = Tk()
 master.wm_title("AbuMoola")
@@ -62,21 +108,15 @@ the_date.grid(row = 3, column = 0, columnspan = 6)
 the_time = Label(master, font = ("Times", 50), foreground='white', bg='black', height = 1)
 the_time.grid(row = 3, column = 3, columnspan = 6)
 time1=""
-def main_clock():
-    global time1
-    time2 = time.strftime("%H:%M:%S")
-    if time2 != time1:
-        time1=time2
-        the_time.config(text=time2, width = 10)
-    the_time.after(200, main_clock)
 
 master.grid_rowconfigure(4, minsize=100)
 
 finance_but = Button(master, text="Finances", font=("system", 50), width = 20, height = 4)#, command=calander)
 finance_but.grid(row=5, column=0, columnspan = 5)
 
-contact_but = Button(master, text="Contacts", font=("system", 50), width = 20, height = 4)#, command=calander)
+contact_but = Button(master, text="Contacts", font=("system", 50), width = 20, height = 4, command = contact_page)
 contact_but.grid(row=5, column=4, columnspan = 5)
+
 
 master.grid_rowconfigure(6, minsize=100)
 
