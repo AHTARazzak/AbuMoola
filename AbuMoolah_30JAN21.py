b3e3ce8ec5f1f7cbdb8f6f0ea11d86c55bc695d2
ternlef11 <aliarazzak@hotmail.com>
2021-01-30 15:09:03 +0000

    Contacts finished :japanese_goblin:

--- AbuMoolah_30JAN21.py
+++ AbuMoolah_30JAN21.py
@@ -17,16 +17,19 @@
 ########### LIBRARIES ##################################
 from tkinter import *
 from tkinter.font import Font
+from tkinter import scrolledtext
 import os.path, pathlib, random, time, os, sys
 from datetime import datetime
 
+########### GLOBALS ##################################
+global the_version
+the_version = "AbuMoola (version 0) by AHTAR"
 
 ########### FUNCTIONS ##################################
-def contact_page():
-    master.destroy()
+def contact_page(page_open):
+    page_open.destroy()
     contacts = Tk()
     contacts.wm_title("AbuMoola - Contacts")
-    #contacts.geometry('1000x800'.format(1000, 800))
     contacts.configure(bg = 'black')
 
     Label(contacts, font = ("Times", 120), foreground = 'white', bg = 'black', text = user_alias + " Contacts").grid(row = 0, column = 1, columnspan = 8)
@@ -34,11 +37,9 @@ def contact_page():
     contacts.grid_rowconfigure(1, minsize = 30)
 
     con_updates = StringVar()
-    #Label(contacts, font = ("Times", 50), foreground = 'white', bg = 'black', text = con_updates).grid(row = 1, column = 1, columnspan = 8)
     con_update_panel = Label(contacts, font = ("Times", 50), foreground = 'white', bg = 'black', textvariable = con_updates).grid(row = 1, column = 1, columnspan = 8)
+
     contacts.grid_rowconfigure(3, minsize = 30)
-    #con_add = Button(contacts, text="+", font=("system", 60), width = 10, height = 2)
-    #con_add.grid(row=2, column=1, columnspan = 5)
 
     Button(contacts, font = ("Times", 50), height = 2, width = 5, text = "Add", command = lambda : contact_add(first_text.get(), last_text.get(), address_text.get(), num_1_text.get(), num_2_text.get(), email_text.get(), insta_text.get(), extra_text.get(), con_updates)).grid(row = 2, column = 0, rowspan = 2)
 
@@ -71,15 +72,47 @@ def contact_page():
     insta_text.set("Instagram")
 
     extra_text = StringVar()
-    extra_ent = Entry(contacts, textvariable = extra_text, font = ('Verdana',60), width = 15).grid(row = 2, column = 5, columnspan = 1)
+    extra_ent = Entry(contacts, textvariable = extra_text, font = ('Verdana',60), width = 15).grid(row = 2, column = 5, columnspan = 2)
     extra_text.set("Extra")
 
-    Button(contacts, font = ("Times", 50), height = 2, width = 5, text = "Add", command = lambda : contact_remove(rem_text.get(), con_updates)).grid(row = 3, column = 5, rowspan = 1)
+    Button(contacts, font = ("Times", 50), height = 1, width = 5, text = "Remove", command = lambda : contact_remove(rem_text.get(), con_updates)).grid(row = 3, column = 5, rowspan = 1)
     rem_text = StringVar()
-    rem_ent = Entry(contacts, textvariable = extra_text, font = ('Verdana',60), width = 15).grid(row = 2, column = 5, columnspan = 1)
+    rem_ent = Entry(contacts, textvariable = rem_text, font = ('Verdana',60), width = 6).grid(row = 3, column = 6, columnspan = 1)
     rem_text.set("ID")
 
+    try:
+        contact_detail_read = open(user_detail_path + "/contacts_details.txt", "r")
+        contacts_to_show = "ID|FirstName|LastName|Addresss|#1|#2|Email|Insta|Extra\n" + contact_detail_read.read()
+        con_output = scrolledtext.ScrolledText(contacts, height=10)
+        con_output.grid(column=1, row=4, columnspan = 6)
+        con_output.configure(background="white", foreground = "black", font = ("Times", 50), wrap= 'word')
+        con_output.insert(END, contacts_to_show)
+    except:
+        con_updates.set("No list of contacts to show")
+
+    contacts.grid_rowconfigure(5, minsize = 20)
+
+    Label(contacts, text = the_version, foreground='white', bg='black', font=("system", 40), height = 1).grid(row=6, column=1, columnspan = 2)
+    Button(contacts, font = ("Times", 50), height = 1, width = 10, text = "Refresh", command = lambda : contact_page(contacts)).grid(row = 6, column = 3, columnspan = 2)
+    Button(contacts, font = ("Times", 50), height = 1, width = 10, text = "Finances", command = lambda : finances_page(contacts)).grid(row = 6, column = 5, columnspan = 2)
+
     contacts.mainloop()
+
+def finances_page(page_open):
+    page_open.destroy()
+    finances = Tk()
+    finances.wm_title("AbuMoola - Finances")
+    finances.configure(bg = 'black')
+
+    Label(finances, font = ("Times", 120), foreground = 'white', bg = 'black', text = user_alias + " Finances").grid(row = 0, column = 1, columnspan = 8)
+
+    finances.grid_rowconfigure(1, minsize = 30)
+
+    fin_updates = StringVar()
+    fin_updates_panel = Label(contacts, font = ("Times", 50), foreground = 'white', bg = 'black', textvariable = fin_updates).grid(row = 1, column = 1, columnspan = 8)
+
+    finances.grid_rowconfigure(3, minsize = 30)
+
 
 def contact_add(first_name, last_name, address, num_1, num_2, email, insta, extra, update_this):
     if (first_name =="First Name"):
@@ -103,7 +136,26 @@ def contact_add(first_name, last_name, address, num_1, num_2, email, insta, extr
             contact_file_open.close()
             update_this.set(first_name + " is your first contact !")
 
-def contact_remove()
+def contact_remove(remove_this, update_this):
+    new_contacts = ""
+    if (remove_this == "ID"):
+        update_this.set("Can't remove default")
+    else:
+        try:
+            contact_detail_read = open(user_detail_path + "/contacts_details.txt", "r")
+            for line in contact_detail_read:
+                checkline = line.split('|')
+                if checkline[0] == remove_this:
+                    this_guy = checkline[1] + " " + checkline[2]
+                    pass
+                else:
+                    new_contacts += line
+            contact_detail_read.close()
+            contact_detail_write = open(user_detail_path + "/contacts_details.txt", "w")
+            contact_detail_write.write(new_contacts)
+            update_this.set(this_guy + "was removed.")
+        except:
+            update_this.set("Something went wrong")
 
 def main_clock():
     global time1
@@ -139,19 +191,10 @@ with open(str(working_directory) + "/random_deco/greetings.txt", "r") as the_gre
     greetings_contents = the_greetings.read().split("\n")
     the_greeting = random.choice(greetings_contents[:-1])
 
-    #def add_con():
-        #add_this =
-    #con_add = Button(contacts, text="+", font=("system", 60), width = 10, height = 2)
-    #con_add.grid(row=2, column=1, columnspan = 5)
-
-    #con_sub = Button(contacts, text="-", font=("system", 60), width = 10, height = 2)
-    #con_sub.grid(row=2, column=5, columnspan = 5)
-
 master = Tk()
 master.wm_title("AbuMoola")
 master.geometry('1000x800'.format(1000, 800))
 master.configure(bg = 'black')
-#master.grid_columnconfigure(0, weight=5)
 now = datetime.now()
 
 title = Label(master, font = ("Times", 80), foreground='white', bg='black', text = str(the_greeting), height = 1, width = 40)
@@ -169,22 +212,19 @@ time1=""
 
 master.grid_rowconfigure(4, minsize=100)
 
-finance_but = Button(master, text="Finances", font=("system", 50), width = 20, height = 4)#, command=calander)
-finance_but.grid(row=5, column=0, columnspan = 5)
+finance_but = Button(master, text="Finances", font=("system", 50), width = 20, height = 4).grid(row=5, column=0, columnspan = 5)
 
-contact_but = Button(master, text="Contacts", font=("system", 50), width = 20, height = 4, command = contact_page)
+contact_but = Button(master, text="Contacts", font=("system", 50), width = 20, height = 4, command = lambda : contact_page(master))
 contact_but.grid(row=5, column=4, columnspan = 5)
 
 
 master.grid_rowconfigure(6, minsize=100)
 
-github_but = Button(master, text="Github", font=("system", 50), width = 51, height = 3)#, command=calander)
-github_but.grid(row=8, column=2, columnspan = 5)
+github_but = Button(master, text="Github", font=("system", 50), width = 51, height = 3).grid(row=8, column=2, columnspan = 5)
 
 master.grid_rowconfigure(9, minsize=120)
 
-github_but = Label(master, text="AbuMoola (version 0) by AHTAR", foreground='white', bg='black', font=("system", 40), height = 1)#, command=calander)
-github_but.grid(row=10, column=0, columnspan = 5)
+Label(master, text = the_version, foreground = 'white', bg = 'black', font = ("system", 40), height = 1).grid(row = 10, column=0, columnspan = 5)
 
 main_clock()
 master.mainloop()
